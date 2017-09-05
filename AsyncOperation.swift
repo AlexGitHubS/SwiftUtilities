dc1d18da6dc7a69558ebf2ba7d8569d8caf9deca
Alex Kozachenko <alex.iMail@icloud.com>
2017-09-04 23:12:42 -0400

    AsyncOperation.swift added

---
+++ AsyncOperation.swift
@@ -0,0 +1,41 @@
+//
+//  AsyncOperation.swift
+//  ttc
+//
+//  Created by Alex on 2017-09-03.
+//  Copyright © 2017 Alex Kozachenko. All rights reserved.
+//
+
+import Foundation
+
+class AsynchronousOperation : Operation {
+    enum State: String {
+        case ready = "Ready"
+        case executing = "Executing"
+        case finished = "Finished"
+        fileprivate var keyPath: String { return "is" + self.rawValue }
+    }
+
+    override var isAsynchronous: Bool { return true }
+    override var isExecuting: Bool { return state == .executing }
+    override var isFinished: Bool { return state == .finished }
+    
+    var state = State.ready {
+        willSet {
+            willChangeValue(forKey: state.keyPath)
+            willChangeValue(forKey: newValue.keyPath)
+        }
+        didSet {
+            didChangeValue(forKey: state.keyPath)
+            didChangeValue(forKey: oldValue.keyPath)
+        }
+    }
+
+    func finish() {
+        state = .finished
+    }
+    func execute() {
+        state = .executing
+    }
+    
+}
