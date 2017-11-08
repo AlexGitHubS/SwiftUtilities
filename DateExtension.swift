daa554d4ad0dd4350f8a6afcd3697f725fa3dd2b
Alex Kozachenko <alex.iMail@icloud.com>
2017-11-07 21:57:06 -0500

    init commit

--- DateExtension.swift
+++ DateExtension.swift
@@ -5,11 +5,7 @@ import Foundation
 
 extension Date {
     
-<<<<<<< HEAD
     public enum dayName: Int {
-=======
-    enum dayName: Int {
->>>>>>> 977b31ff8662e426bdf8e4657ce997f25811b015
         case Sunday = 1, Monday, Tuesday, Wednesday, Thursday, Friday, Saturday
     }
     
@@ -26,12 +22,8 @@ extension Date {
         case October = "October"
         case November = "November"
         case December = "December"
-        
-<<<<<<< HEAD
+    
         public func number() -> Int {
-=======
-        func number() -> Int {
->>>>>>> 977b31ff8662e426bdf8e4657ce997f25811b015
             switch self {
             case .January:
                 return 1
@@ -64,12 +56,7 @@ extension Date {
     /// Default values of timeZone and Calendar for DateFormater are user's current settings & location
     /// TimeZone.knownTimeZoneIdentifiers - all string values for timeZone
     
-<<<<<<< HEAD
     public func toString(dateStyle: DateFormatter.Style = DateFormatter.Style.short, timeStyle: DateFormatter.Style = .none, fromCalendar: Calendar.Identifier = .gregorian , forTimeZone: TimeZone = .current) -> String {
-=======
-    func toString(dateStyle: DateFormatter.Style = DateFormatter.Style.short, timeStyle: DateFormatter.Style = .none, fromCalendar: Calendar.Identifier = .gregorian , forTimeZone: TimeZone = .current) -> String {
->>>>>>> 977b31ff8662e426bdf8e4657ce997f25811b015
-        
         let formater = DateFormatter()
         formater.dateStyle = dateStyle
         formater.timeStyle = timeStyle
@@ -78,12 +65,7 @@ extension Date {
         
         return formater.string(from: self)
     }
-    
-<<<<<<< HEAD
     public func toString(withFormat: String) -> String {
-=======
-    func toString(withFormat: String) -> String {
->>>>>>> 977b31ff8662e426bdf8e4657ce997f25811b015
         
         let formater = DateFormatter()
         formater.calendar = Calendar.current
@@ -93,22 +75,15 @@ extension Date {
     }
     
     ///returns the same `Date` with hour, minute and second set to 00:00:00
-<<<<<<< HEAD
     public func startOfDay(timeZone: TimeZone = .current) -> Date {
-=======
-    func startOfDay(timeZone: TimeZone = .current) -> Date {
->>>>>>> 977b31ff8662e426bdf8e4657ce997f25811b015
+
         let components = self.dateComponentsFromDate([.day,.month,.year])
         let startDate = Date.dateFromComponents(year: components.year!, month: components.month!, day: components.day!, hour: 0, minute: 0, second: 0, timeZone: timeZone)
         
         return startDate!
     }
     ///returns the same `Date` with hour, minute and second set to 23:59:59
-<<<<<<< HEAD
     public func endOfDay(timeZone: TimeZone = .current) -> Date {
-=======
-    func endOfDay(timeZone: TimeZone = .current) -> Date {
->>>>>>> 977b31ff8662e426bdf8e4657ce997f25811b015
         let components = self.dateComponentsFromDate([.day,.month,.year])
         let startDate = Date.dateFromComponents(year: components.year!, month: components.month!, day: components.day!, hour: 23, minute: 59, second: 59, timeZone: timeZone)
         
@@ -117,47 +92,27 @@ extension Date {
     
     //ex: "Apr 21, 2016, 1:19 PM"
     ///returns the `Date` that was exactly 24 hours ago
-<<<<<<< HEAD
     public func yesturday() -> Date {
-=======
-    func yesturday() -> Date {
->>>>>>> 977b31ff8662e426bdf8e4657ce997f25811b015
         return self.addingTimeInterval(-86400.0)
     }
     
     //ex: "Apr 23, 2016, 1:19 PM"
     ///returns the `Date` that will be exactly 24 hours since date
-<<<<<<< HEAD
     public func tomorrow() -> Date {
-=======
-    func tomorrow() -> Date {
->>>>>>> 977b31ff8662e426bdf8e4657ce997f25811b015
         return self.addingTimeInterval(86400.0)
     }
     
     ///returns the `Date` that was N days ago
-<<<<<<< HEAD
     public func daysAgo(_ days: Double) -> Date {
         return self.addingTimeInterval(-86400.0 * days)
     }
     ///returns the `Date` that will be in N days
     public func inDays(_ days: Double) -> Date {
-=======
-    func daysAgo(_ days: Double) -> Date {
-        return self.addingTimeInterval(-86400.0 * days)
-    }
-    ///returns the `Date` that will be in N days
-    func inDays(_ days: Double) -> Date {
->>>>>>> 977b31ff8662e426bdf8e4657ce997f25811b015
         return self.addingTimeInterval(86400.0 * days)
     }
     
     ///returns array of specified `DateComponents` from the `Date`
-<<<<<<< HEAD
     public func dateComponentsFromDate(_ withComponents: Set<Calendar.Component>) -> DateComponents {
-=======
-    func dateComponentsFromDate(_ withComponents: Set<Calendar.Component>) -> DateComponents {
->>>>>>> 977b31ff8662e426bdf8e4657ce997f25811b015
         let calendar = Calendar(identifier: Calendar.Identifier.gregorian)
         let components = (calendar as Calendar).dateComponents(withComponents, from: self)
         return components
@@ -165,31 +120,19 @@ extension Date {
     
     //MARK:- COMPONENTS FROM DATE
     ///returns number of day 1-7 from the `Date`
-<<<<<<< HEAD
     public func day() -> Int {
-=======
-    func day() -> Int {
->>>>>>> 977b31ff8662e426bdf8e4657ce997f25811b015
         let selfComponents = self.dateComponentsFromDate([.day])
         guard let day = selfComponents.day else { return 0 }
         return day
     }
     ///returns number of day 1-12 from the `Date`
-<<<<<<< HEAD
     public func month() -> Int {
-=======
-    func month() -> Int {
->>>>>>> 977b31ff8662e426bdf8e4657ce997f25811b015
         let selfComponents = self.dateComponentsFromDate([.month])
         guard selfComponents.month != nil else { return 0 }
         return selfComponents.month!
     }
     ///returns the year from the `Date`
-<<<<<<< HEAD
     public func year() -> Int {
-=======
-    func year() -> Int {
->>>>>>> 977b31ff8662e426bdf8e4657ce997f25811b015
         let selfComponents = self.dateComponentsFromDate([.year])
         guard selfComponents.year != nil else { return 0}
         return selfComponents.year!
@@ -198,11 +141,7 @@ extension Date {
     //MARK:- COMPARING METHODS
     
     /// returns true if the `Date`'s day is the same as a day name specified
-<<<<<<< HEAD
     public func isDay(dayName: dayName) -> Bool {
-=======
-    func isDay(dayName: dayName) -> Bool {
->>>>>>> 977b31ff8662e426bdf8e4657ce997f25811b015
         let calendar = Calendar(identifier: Calendar.Identifier.gregorian)
         let dayNumber = (calendar as Calendar).component(Calendar.Component.weekday, from: self)
         
@@ -210,11 +149,7 @@ extension Date {
     }
     
     ///returns true if `Date` is the same or was before the specified `Date`. Compares only day, month and year.
-<<<<<<< HEAD
     public func lessThanOrEqualTo(date secondDate: Date) -> Bool {
-=======
-    func lessThanOrEqualTo(date secondDate: Date) -> Bool {
->>>>>>> 977b31ff8662e426bdf8e4657ce997f25811b015
         let selfComponents = self.dateComponentsFromDate([.year,.month,.day])
         let targetComponents = secondDate.dateComponentsFromDate([.year,.month,.day])
         
@@ -232,11 +167,7 @@ extension Date {
         }
     }
     ///returns true if `Date` was before the specified `Date`. Compares only day, month and year.
-<<<<<<< HEAD
     public func lessThan(date secondDate: Date) -> Bool {
-=======
-    func lessThan(date secondDate: Date) -> Bool {
->>>>>>> 977b31ff8662e426bdf8e4657ce997f25811b015
         let selfComponents = self.dateComponentsFromDate([.year,.month,.day])
         let targetComponents = secondDate.dateComponentsFromDate([.year,.month,.day])
         
@@ -255,11 +186,7 @@ extension Date {
         }
     }
     ///returns true if `Date` is the same or comes after the specified `Date`. Compares only day, month and year.
-<<<<<<< HEAD
     public func greaterThanOrEqual(date secondDate: Date) -> Bool {
-=======
-    func greaterThanOrEqual(date secondDate: Date) -> Bool {
->>>>>>> 977b31ff8662e426bdf8e4657ce997f25811b015
         let selfComponents = self.dateComponentsFromDate([.year,.month,.day])
         let targetComponents = secondDate.dateComponentsFromDate([.year,.month,.day])
         
@@ -278,11 +205,7 @@ extension Date {
         }
     }
     ///returns true if `Date` comes after the specified `Date`. Compares only day, month and year.
-<<<<<<< HEAD
     public func greaterThan(date secondDate: Date) -> Bool {
-=======
-    func greaterThan(date secondDate: Date) -> Bool {
->>>>>>> 977b31ff8662e426bdf8e4657ce997f25811b015
         let selfComponents = self.dateComponentsFromDate([.year,.month,.day])
         let targetComponents = secondDate.dateComponentsFromDate([.year,.month,.day])
         
@@ -301,11 +224,7 @@ extension Date {
         }
     }
     ///returns true if day month and year matches to the `Date`
-<<<<<<< HEAD
     public func isSameDay(date: Date) -> Bool {
-=======
-    func isSameDay(date: Date) -> Bool {
->>>>>>> 977b31ff8662e426bdf8e4657ce997f25811b015
         let selfComponents = self.dateComponentsFromDate([.year,.month,.day])
         let targetComponents = date.dateComponentsFromDate([.year,.month,.day])
         
@@ -319,12 +238,7 @@ extension Date {
     //MARK:- Static methods
     
     ///returns number of dates between two specified `Date`s. Inclusive by default (including
-    
-<<<<<<< HEAD
     static public func NumberOfDaysBetweenDates(fromDate date1: Date, toDate date2: Date, inclusive: Bool = true) -> Int {
-=======
-    static func NumberOfDaysBetweenDates(fromDate date1: Date, toDate date2: Date, inclusive: Bool = true) -> Int {
->>>>>>> 977b31ff8662e426bdf8e4657ce997f25811b015
         var nDays = 0
         
         if date1.isSameDay(date: date2) { return nDays }
@@ -348,11 +262,7 @@ extension Date {
     }
     
     ///Returns string formated Date in specified format. 'dd-MMM-yy' by defaylt
-<<<<<<< HEAD
     static public func getStringFromDate(date: Date, format: String = "dd-MMM-yy") -> String {
-=======
-    static func getStringFromDate(date: Date, format: String = "dd-MMM-yy") -> String {
->>>>>>> 977b31ff8662e426bdf8e4657ce997f25811b015
         let formater = DateFormatter()
         formater.dateFormat = format
         let orderDate = formater.string(from: date)
@@ -360,11 +270,7 @@ extension Date {
     }
     
     ///Creates a `Date` from specified components.
-<<<<<<< HEAD
-    static public func dateFromComponents(year: Int, month: Int, day: Int, hour: Int = 12, minute: Int = 0, second: Int = 0, timeZone tZ: TimeZone = .current) -> Date? {
-=======
-    static func dateFromComponents(year: Int, month: Int, day: Int, hour: Int = 12, minute: Int = 0, second: Int = 0, timeZone tZ: TimeZone = .current) -> Date? {
->>>>>>> 977b31ff8662e426bdf8e4657ce997f25811b015
+    public static  func dateFromComponents(year: Int, month: Int, day: Int, hour: Int = 12, minute: Int = 0, second: Int = 0, timeZone tZ: TimeZone = .current) -> Date? {
         // Specify date components
         var dateComponents = DateComponents()
         
@@ -384,11 +290,7 @@ extension Date {
     }
     
     ///Creates a `Date` in a specified format. Default is "dd/MM/yyyy" format
-<<<<<<< HEAD
     static public func dateFromString(date: String, format: String = "dd/MM/yyyy") -> Date? {
-=======
-    static func dateFromString(date: String, format: String = "dd/MM/yyyy") -> Date? {
->>>>>>> 977b31ff8662e426bdf8e4657ce997f25811b015
         let formatter = DateFormatter()
         formatter.dateFormat = format
         let someDateTime = formatter.date(from: date)
@@ -396,11 +298,7 @@ extension Date {
     }
     
     ///returns Date in "dd/MM/yyyy-hh:mm aa" format
-<<<<<<< HEAD
     static public func dateFromStringFromTimeZone(date: String, format: String = "dd/MM/yyyy-hh:mm aa", timeZone: String = "GMT+0:00") -> Date? {
-=======
-    static func dateFromStringFromTimeZone(date: String, format: String = "dd/MM/yyyy-hh:mm aa", timeZone: String = "GMT+0:00") -> Date? {
->>>>>>> 977b31ff8662e426bdf8e4657ce997f25811b015
         let formater = DateFormatter()
         formater.dateFormat = format
         formater.timeZone = TimeZone(identifier: timeZone)
@@ -408,7 +306,6 @@ extension Date {
         return formater.date(from: date)
     }
     
-<<<<<<< HEAD
     static public func minutesIn(days: Double) -> Double {
         return days*24*60
     }
@@ -422,32 +319,13 @@ extension Date {
         return hours*60*60
     }
     static public func secondsIn(minutes: Double) -> Double {
-=======
-    static func minutesIn(days: Double) -> Double {
-        return days*24*60
-    }
-    static func minutesIn(hours: Double) -> Double {
-        return hours*60
-    }
-    static func secondsIn(days: Double) -> Double {
-        return days*24*60*60
-    }
-    static func secondsIn(hours: Double) -> Double {
-        return hours*60*60
-    }
-    static func secondsIn(minutes: Double) -> Double {
->>>>>>> 977b31ff8662e426bdf8e4657ce997f25811b015
         return minutes*60
     }
     
     //MARK: TIME ZONE NAMES
     
     ///return all European Time Zone Identifiers
-<<<<<<< HEAD
     static public func EuropeanTimeZoneNames() -> [String] {
-=======
-    static func EuropeanTimeZoneNames() -> [String] {
->>>>>>> 977b31ff8662e426bdf8e4657ce997f25811b015
         return TimeZone.knownTimeZoneIdentifiers.filter { (zName) in
             return zName.hasPrefix("Euro")
         }
@@ -455,7 +333,6 @@ extension Date {
     
     //MARK:- Comparison methods overload
     ///overload method for greaterThan(_:) method. Returns true if lhs `Date` comes after rhs `Date`. (Compares Year, Month, Day)
-<<<<<<< HEAD
     static public func >(lhs: Date, rhs: Date) -> Bool {
         return lhs.greaterThan(date: rhs)
     }
@@ -485,37 +362,6 @@ extension Date {
     }
     ///returns true if lhs `Date` is the same as rhs `Date` including time.
     static public func ===(lhs: Date, rhs: Date) -> Bool {
-=======
-    static func >(lhs: Date, rhs: Date) -> Bool {
-        return lhs.greaterThan(date: rhs)
-    }
-    ///overload method for lessThan(_:) method. Returns true if lhs `Date` comes before rhs `Date`. (Compares Year, Month, Day)
-    static func <(lhs: Date, rhs: Date) -> Bool {
-        return lhs.lessThan(date: rhs)
-    }
-    ///overload method for greaterThanOrEqual(_:) method. Returns true if lhs `Date` comes after rhs `Date` or is the same `Date` as rhs `Date`. (Compares Year, Month, Day)
-    static func >=(lhs: Date, rhs: Date) -> Bool {
-        return lhs.greaterThanOrEqual(date: rhs)
-    }
-    ///overload method for lessThanOrEqualTo(_:) method. Returns true if lhs `Date` comes before rhs `Date` or is the same `Date` as rhs `Date`. (Compares Year, Month, Day)
-    static func <=(lhs: Date, rhs: Date) -> Bool {
-        return lhs.lessThanOrEqualTo(date: rhs)
-    }
-    ///returns true if lhs `Date` comes after rhs `Date` including time.
-    static func >>(lhs: Date, rhs: Date) -> Bool {
-        return lhs.timeIntervalSinceReferenceDate > rhs.timeIntervalSinceReferenceDate
-    }
-    ///returns true if lhs `Date` comes before rhs `Date` including time.
-    static func <<(lhs: Date, rhs: Date) -> Bool {
-        return lhs.timeIntervalSinceReferenceDate < rhs.timeIntervalSinceReferenceDate
-    }
-    ///overload method for isSameDay(_:) method. Returns true if lhs `Date`'s day is a same that rhs `Date`'s day. (Compares Year, Month, Day)
-    static func ==(lhs: Date, rhs: Date) -> Bool {
-        return lhs.isSameDay(date: rhs)
-    }
-    ///returns true if lhs `Date` is the same as rhs `Date` including time.
-    static func ===(lhs: Date, rhs: Date) -> Bool {
->>>>>>> 977b31ff8662e426bdf8e4657ce997f25811b015
         return lhs.timeIntervalSinceReferenceDate == rhs.timeIntervalSinceReferenceDate
     }
 }
